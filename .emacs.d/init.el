9d8bdaee6f6569b6b42b04ea9508d5b8de337adf
Kyle Mahan <kyle.mahan@gmail.com>
2013-07-31 18:36:10 -0700

    Added init.el as a real file rather than a symlink

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,1 +1,45 @@
-/home/kmahan/.emacs.d/init.el
+(require 'package)
+(add-to-list 'package-archives
+  '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+(defvar ffip-project-file '(".hg" ".git"))
+(defvar ffip-limit 4096)
+(defvar ffip-patterns '("*.java" "*.cpp" "*.h" "*.xml" "*.gradle" "*.txt"))
+
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+(global-set-key (kbd "<f7>") 'recompile)
+
+(global-set-key (kbd "C-x f") 'ffip)
+
+(add-to-list 'auto-mode-alist '("\\.gradle\\'" . groovy-mode))
+(add-to-list 'auto-mode-alist '("CMakeLists\\.txt\\'" . cmake-mode))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(custom-enabled-themes (quote (tango-dark)))
+ '(custom-safe-themes (quote ("967c58175840fcea30b56f2a5a326b232d4939393bed59339d21e46cf4798ecf" default)))
+ '(inhibit-startup-screen t))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Ubuntu Mono" :foundry "unknown"
+                        :slant normal :weight normal
+                        :height 98 :width normal)))))
+
+(defvar my-packages '(better-defaults find-file-in-project smex))
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
