99e2f7642991b4c30dc2f44e2838b534efb8fccc
Kyle Mahan <kmahan@lemur>
2013-09-03 10:48:54 -0700

    Modified emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -19,24 +19,12 @@
 (add-to-list 'auto-mode-alist '("\\.gradle\\'" . groovy-mode))
 (add-to-list 'auto-mode-alist '("CMakeLists\\.txt\\'" . cmake-mode))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(column-number-mode t)
- '(custom-enabled-themes (quote (tango-dark)))
- '(custom-safe-themes (quote ("967c58175840fcea30b56f2a5a326b232d4939393bed59339d21e46cf4798ecf" default)))
- '(inhibit-startup-screen t))
+(setq column-number-mode t)
+(setq inhibit-startup-screen t)
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Ubuntu Mono" :foundry "unknown"
-                        :slant normal :weight normal
-                        :height 98 :width normal)))))
+(if window-system
+    (load-theme 'tango-dark))
+
 
 (defvar my-packages '(better-defaults find-file-in-project smex))
 
