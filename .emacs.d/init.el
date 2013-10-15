1aa8d67302789d4b2885505ac4ed69a0f59b69a5
Kyle Mahan <kyle.mahan@gmail.com>
2013-10-14 17:32:25 -0700

    some customizations

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,7 +5,9 @@
 
 (defvar my-packages '(better-defaults
                       find-file-in-project smex
-                      soft-charcoal-theme))
+                      soft-charcoal-theme
+                      buffer-move
+                      highlight-symbol))
 
 (when (not package-archive-contents)
   (package-refresh-contents))
@@ -23,14 +25,31 @@
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 ;; This is your old M-x.
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+(global-set-key (kbd "<f6>") 'compile)
 (global-set-key (kbd "<f7>") 'recompile)
 (global-set-key (kbd "C-x f") 'ffip)
 
+(global-set-key (kbd "<C-S-up>")     'buf-move-up)
+(global-set-key (kbd "<C-S-down>")   'buf-move-down)
+(global-set-key (kbd "<C-S-left>")   'buf-move-left)
+(global-set-key (kbd "<C-S-right>")  'buf-move-right)
+
+;; F3 highlight symbols like vim's *
+(add-hook 'prog-mode-hook (lambda () (highlight-symbol-mode)))
+(setq highlight-symbol-on-navigation-p t)
+(global-set-key [f3] 'highlight-symbol-next)
+(global-set-key [(shift f3)] 'highlight-symbol-prev)
+
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 (add-to-list 'auto-mode-alist '("\\.gradle\\'" . groovy-mode))
 (add-to-list 'auto-mode-alist '("CMakeLists\\.txt\\'" . cmake-mode))
 
-(setq column-number-mode t)
-(setq inhibit-startup-screen t)
+(setq column-number-mode t
+      inhibit-startup-screen t
+      safe-local-variable-values '((eval highlight-regexp "	")
+                                   (c-default-style "linux"))
+      custom-safe-themes t
+      vc-follow-symlinks nil)
 
 (setq load-path
       (cons (expand-file-name "C:\\Apps\\CMake 2.8\\share\\cmake-2.8\\editors\\emacs")
@@ -41,8 +60,6 @@
       (append '(("CMakeLists\\.txt\\'" . cmake-mode)
                 ("\\.cmake\\'" . cmake-mode))
               auto-mode-alist))
-
-(setq custom-safe-themes t)
 
 (if window-system
     (progn
