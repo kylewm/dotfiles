0b0e877548c1c9c8566f8bde48bb7c8eb2dbb7e1
Kyle Mahan <kmahan@KMlinux.(none)>
2013-12-20 18:01:50 -0800

    Clean up emacs stuff

--- init.el
+++ init.el
@@ -98,47 +98,9 @@
 (autoload 'ctags-create-or-update-tags-table "ctags" "Ctags generation and navigation" t)
 
 
-;; TODO move to a separate configuration file
-(autoload 'mu4e "mu4e")
-
-(setq
- mu4e-maildir       "~/Mail/"
- mu4e-sent-folder   "/kylewm/Sent"
- mu4e-drafts-folder "/kylewm/Drafts"
- mu4e-trash-folder  "/kylewm/Trash"
- mu4e-refile-folder "/kylewm/Archive"
- user-mail-address "kyle@kylewm.com"
- smtpmail-default-smtp-server "orin.kylewm.com"
- smtpmail-local-domain "kylewm.com"
- smtpmail-smtp-server "orin.kylewm.com"
- smtpmail-stream-type 'starttls
- smtpmail-smtp-service 587)
-
 
 (if window-system
     (progn
       (load-theme 'sanityinc-tomorrow-night)
       (set-face-font 'default (if (eq window-system 'w32)
                                   "Consolas-10" "Liberation Mono-10"))))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
- '(ansi-color-names-vector ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
- '(custom-enabled-themes (quote (soft-charcoal)))
- '(fci-rule-color "#383838")
- '(safe-local-variable-values (quote ((c-default-style "linux") (eval highlight-regexp "	"))))
- '(send-mail-function (quote smtpmail-send-it))
- '(vc-annotate-background "#2B2B2B")
- '(vc-annotate-color-map (quote ((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F") (80 . "#D0BF8F") (100 . "#E0CF9F") (120 . "#F0DFAF") (140 . "#5F7F5F") (160 . "#7F9F7F") (180 . "#8FB28F") (200 . "#9FC59F") (220 . "#AFD8AF") (240 . "#BFEBBF") (260 . "#93E0E3") (280 . "#6CA0A3") (300 . "#7CB8BB") (320 . "#8CD0D3") (340 . "#94BFF3") (360 . "#DC8CC3"))))
- '(vc-annotate-very-old-color "#DC8CC3"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(put 'downcase-region 'disabled nil)
-(put 'upcase-region 'disabled nil)
